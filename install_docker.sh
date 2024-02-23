71fb08368e1c031d50dde12907e96602a98c3aa5
sachidanadpc <49983832+sachidanadpc@users.noreply.github.com>
2024-02-23 15:05:45 +0530

    Update install_docker.sh
    updated all steps to install docker in proper way

--- install_docker.sh
+++ install_docker.sh
@@ -3,19 +3,8 @@
 # Update the package list
 sudo yum update -y
 
-# Install dependencies
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-
-# Add Docker repository if not already added
-if ! yum repolist | grep -q 'docker.*centos'; then
-    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-fi
-
-# Install Docker if not already installed
-if ! rpm -q docker-ce docker-ce-cli containerd.io &> /dev/null; then
-    echo "Installing Docker..."
-    sudo yum install -y docker-ce docker-ce-cli containerd.io
-fi
+# Install docker
+sudo yum install -y docker
 
 # Start and enable Docker service if not already started
 if ! systemctl is-active --quiet docker; then
@@ -32,25 +21,12 @@ fi
 # Install docker-compose if not already installed
 if ! command -v docker-compose &> /dev/null; then
     echo "Installing docker-compose..."
-    sudo yum install -y docker-compose
-fi
-
-# Install Docker Metadata if not already installed
-if ! command -v docker-metadata &> /dev/null; then
-    echo "Installing Docker Metadata..."
-    sudo yum install -y docker-latest-metadata
-fi
-
-# Install Docker CLI if not already installed
-if ! command -v docker-latest &> /dev/null; then
-    echo "Installing Docker CLI..."
-    sudo yum install -y docker-latest
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose    
 fi
 
 # Print Docker, Docker Compose, Docker Metadata, and Docker CLI versions
 docker --version
 docker-compose --version
-docker-metadata --version
-docker-latest --version
 
-echo "Docker, Docker Compose, Docker Metadata, and Docker CLI installed successfully."
+echo "Docker, Docker Compose installed successfully."
